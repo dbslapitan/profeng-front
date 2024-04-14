d6c5568dfedc83bc7f03569c7a2633a5155d4e45
dbslapitan <dbslapitan@gmail.com>
2024-04-14 02:08:59 -0400

    finish styiling the reading quiz

--- src/app/quiz/reading/[id]/page.tsx
+++ src/app/quiz/reading/[id]/page.tsx
@@ -1,6 +1,7 @@
 import { ReadingTemplate } from "@/models/reading.model";
 import axios from "axios";
 import { postReadingAnswer } from "@/utils/actions";
+import style from './reading-quiz.module.scss';
 
 const API = process.env.API;
 
@@ -9,31 +10,30 @@ export default async function Reading({ params }: { params: { id: string } }) {
         const { data } = await axios.get<ReadingTemplate>(`${API}/api/v1/reading/${params.id}`);
 
     return (
-        <>
-            <h1>{data.title}</h1>
+        <section className={`${style["reading"]}`}>
+            <h1 className={`page-header ${style["reading__title"]}`}>{data.title}</h1>
             {
                 data.content.map((paragraph, index) => {
                     return (
-                        <p key={index}>{paragraph}</p>
+                        <p className={`regular ${style["reading__paragraph"]}`} key={index}>{paragraph}</p>
                     );
                 })
             }
 
-            <h3>Questions: </h3>
-
-            <form action={postReadingAnswer}>
+            <form className={`${style["question"]}`} action={postReadingAnswer}>
+                <h2 className={`section-header ${style["question__title"]}`}>Questions: </h2>
                 <input type="text" name="count" id="count" defaultValue={data.questions.length} hidden />
                 <input type="text" name="reading" id="reading" defaultValue={data._id} hidden />
-                <ol>
+                <ol className={`regular ${style["question__prompts"]}`}>
                     {
                         data.questions.map((qnc, qncIndex) => {
-                            return (<li key={qncIndex}>
+                            return (<li className={`${style["question__set"]}`} key={qncIndex}>
                                 <p>{qnc.question}</p>
                                 <ul>
                                     {
                                         qnc.options.map((option, choiceIndex) => {
                                             return (
-                                                <li key={choiceIndex}>
+                                                <li className={`${style["question__choice"]}`} key={choiceIndex}>
                                                     <input type="radio" name={`question-${qncIndex}`} id={`q${qncIndex}-option-${choiceIndex}`} defaultValue={option} /> <label htmlFor={`q${qncIndex}-option-${choiceIndex}`}>{option}</label>
                                                 </li>
                                             );
@@ -46,9 +46,9 @@ export default async function Reading({ params }: { params: { id: string } }) {
                         })
                     }
                 </ol>
-                <button>Submit</button>
+                <button className={`${style["question__submit"]}`}>Submit</button>
             </form>
 
-        </>
+        </section>
     );
 }
