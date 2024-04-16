743549ba74351c7def88b07b89861ee37c67adc6
dbslapitan <dbslapitan@gmail.com>
2024-04-16 02:02:59 -0400

    move fonts to use its own file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,14 +3,12 @@ import "./globals.scss";
 import HeaderNav from "@/components/header-nav/HeaderNav";
 import { Suspense } from "react";
 import Loading from "./loading";
-import { Titillium_Web } from "next/font/google";
+import {titilium} from "../libs/fonts";
 
 export const metadata: Metadata = {
     title: "Profeng",
     description: "ProfEng(Profecient English) is a web application that aims to increase the English Proficiency of users. It leverages the power of AI through ChatGPT to provide real-time feedback empowering users to enhance their proficiency in English effectively and confidently.",
 };
-
-export const titilium = Titillium_Web({weight: ["400", "600", "700", "900"], subsets: ["latin"]})
 
 export default function RootLayout({
     children,
